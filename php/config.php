3da17de5912e0382ec9c9b6d5141f736db283997
nongp <nongp@hotmail.com>
2017-11-19 12:17:12 +0700

    Update config.php

--- php/config.php
+++ php/config.php
@@ -3,5 +3,5 @@
 require_once 'vendor/autoload.php';
 
 /* Defined OMISE KEYS */
-define('OMISE_PUBLIC_KEY', 'pkey_test');
-define('OMISE_SECRET_KEY', 'skey_test');
+define('OMISE_PUBLIC_KEY', 'pkey_test_53x6wu9xs4h7zqkzq37');
+define('OMISE_SECRET_KEY', 'skey_test_53x6wu9xtwffpegkhvk');
